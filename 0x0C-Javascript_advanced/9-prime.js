3b9d1c71af6760f2aab041de55d1a0a374f5abbe
Camilo <trresqwe@gmail.com>
2021-06-22 21:38:07 -0500

    Update

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,32 @@
+function countPrimeNumbers(){
+    let numOfPrimeNums = 0
+
+    function isPrime(number) {
+        status = 1;
+        if (number < 2) {
+            status = 0;
+        }
+        else {
+            for (let i = 2; i < number; i++) {
+                if (number % i === 0) {
+                    status = 0;
+                }
+            }
+        }
+        return status
+    }
+
+    for (let num = 2; num < 101; num++) {
+        if (isPrime(num) == 1) {
+            numOfPrimeNums += 1
+        }
+    }
+
+    return numOfPrimeNums;
+}
+
+let start = window.performance.now();
+countPrimeNumbers();
+let end = window.performance.now();
+
+console.log("Execution time of printing countPrimeNumbers was " + (end - start) + " milliseconds.");
